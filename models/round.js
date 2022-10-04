77f1a15817dce3b8d6d43ce4dc5d867e4649497d
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-10-03 18:22:35 -0700

    fix round update to allow for partially complete round

--- models/round.js
+++ models/round.js
@@ -50,7 +50,7 @@ class Round {
     const { courseRating, courseSlope } = courseRes.rows[0];
 
     /*******  Compute player_index by averaging lowest 2 of last 4 score diffs  ************/
-    const roundsRes = await db.query(
+    const scoreDiffRes = await db.query(
       `SELECT tournament_date AS "tournamentDate",
               score_differential AS "scoreDifferential" 
               FROM rounds 
@@ -60,7 +60,7 @@ class Round {
     );
 
     // make array of last 4 scoreDifferentials ['18.3', '22.1', '19.5', '24.4']
-    const scoreDiffsArr = roundsRes.rows.map((r) => +r.scoreDifferential);
+    const scoreDiffsArr = scoreDiffRes.rows.map((r) => +r.scoreDifferential);
 
     //if this is the players first round start them with score diff of 0
     if (scoreDiffsArr.length === 0) {
@@ -73,6 +73,8 @@ class Round {
     let netStrokes = null;
     let playerIndex;
     let courseHandicap;
+
+    //Hard to account for player with less than 4 rounds without WET code
 
     ///////////////// IF THE ROUND IS FULLY COMPLETED ///////////////////////
     /******** calculate the score differential and net strokes  ******/
@@ -127,7 +129,7 @@ class Round {
       console.log("COURSE HANDICAP", courseHandicap);
     }
 
-    /*** ALL TABLE INSERTION HAPPENS BELOW THE CONDITIONALS THAT SET ROUND COLUMN VALUES */
+    /*** ALL TABLE INSERTION HAPPENS BELOW THE CONDITIONALS THAT COMPUTE ROUND COLUMN VALUES */
     // Insert into rounds table first and grab the round_id
     const roundRes = await db.query(
       `INSERT INTO rounds
@@ -149,32 +151,26 @@ class Round {
         netStrokes,
       ]
     );
-    // console.log("ROUND ID", roundRes.rows[0].id);
 
-    const roundId = roundRes.rows[0].id;
+    const round = roundRes.rows[0];
 
-    //Insert all the strokes for the round
+    /*** Insert all the strokes for the round ***/
     const strokesRes = await db.query(
       `INSERT INTO strokes
       (round_id, hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
       RETURNING hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18`,
-      [roundId, ...Object.values(strokes)]
+      [round.id, ...Object.values(strokes)]
     );
 
-    console.log(strokes);
-    console.log(Object.values(strokes));
-
-    //Insert all the putts for the round
+    /*** Insert all the putts for the round ***/
     const puttsRes = await db.query(
       `INSERT INTO putts
       (round_id, hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
       RETURNING hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18`,
-      [roundId, ...Object.values(putts)]
+      [round.id, ...Object.values(putts)]
     );
-
-    const round = roundRes.rows[0];
 
     round.strokes = strokesRes.rows[0];
     round.putts = puttsRes.rows[0];
@@ -453,15 +449,15 @@ class Round {
     //Throw bad request error if data is empty
     if (Object.keys(data).length === 0) throw new BadRequestError("No data");
 
-    /**  Sum the strokes and putts objects to get the new total_strokes and total_putts */
-    const totalStrokes = Object.values(data.strokes).reduce((a, b) => a + b, 0);
-    const totalPutts = Object.values(data.putts).reduce((a, b) => a + b, 0);
+    const { strokes, putts, username } = data;
+
+    /** STEP 1 : Sum strokes and putts objects to get new total_strokes and total_putts */
+    const totalStrokes = Object.values(strokes).reduce((a, b) => a + b, 0);
+    const totalPutts = Object.values(putts).reduce((a, b) => a + b, 0);
     // console.log("TOTAL STROKES", totalStrokes);
     // console.log("TOTAL PUTTS", totalPutts);
 
-    /** Compute the score_differential for the round (113 / course_slope) * (total_strokes - course_rating) */
-
-    //Query db to grab courseRating and courseSlope
+    /** Query courses table for rating and slope */
     const courseRes = await db.query(
       `SELECT rating AS "courseRating", slope AS "courseSlope"
         FROM rounds 
@@ -470,72 +466,91 @@ class Round {
         WHERE rounds.id = $1`,
       [id]
     );
+    if (!courseRes.rows[0]) throw new NotFoundError(`No round id: ${id}`);
 
-    const course = courseRes.rows[0];
-    if (!course) throw new NotFoundError(`No round id: ${id}`);
+    const { courseRating, courseSlope } = courseRes.rows[0];
 
-    const { courseRating, courseSlope } = course;
-
-    const scoreDifferential = +(
-      (113 / courseSlope) *
-      (totalStrokes - courseRating)
-    ).toFixed(1);
-    // console.log("SCORE DIFFERENTIAL", scoreDifferential);
-
-    /** Compute the player_index by querying the last 4 rounds for the player and taking the average of the lowest 2
-     *  score_differentials (includes the current round scoring_differential) */
-    //find the username using the roundId
-    const usernameRes = await db.query(
-      `SELECT username
-        FROM rounds
-        WHERE id=$1`,
-      [id]
-    );
-
-    const username = usernameRes.rows[0].username;
-
-    // ORDER BY ASC very important because we are using .pop on scoreDiffsArr
-    const roundsRes = await db.query(
+    /*******  Compute player_index by averaging lowest 2 of last 4 score diffs
+     * NOT INCLUDING THE ROUND BEING EDITED BY USING OFFSET! ************/
+    const scoreDiffRes = await db.query(
       `SELECT tournament_date AS "tournamentDate",
-                  score_differential AS "scoreDifferential" 
-                  FROM rounds 
-                  WHERE username=$1 AND score_differential IS NOT NULL
-                  ORDER BY tournament_date ASC LIMIT 4`,
+                      score_differential AS "scoreDifferential" 
+                      FROM rounds 
+                      WHERE username=$1 
+                      ORDER BY tournament_date DESC LIMIT 4 OFFSET 1`,
       [username]
     );
 
     // make array of last 4 scoreDifferentials ['18.3', '22.1', '19.5', '24.4']
-    const scoreDiffsArr = roundsRes.rows.map((r) => +r.scoreDifferential);
-    // Since this is updating round data, pop off the last scoreDiff which is now invalid
-    scoreDiffsArr.pop();
-    //push current round scoreDifferential onto scoreDiffsArr
-    scoreDiffsArr.push(scoreDifferential);
-    // console.log("SCORE DIFFS ARRAY", scoreDiffsArr);
+    const scoreDiffsArr = scoreDiffRes.rows.map((r) => +r.scoreDifferential);
 
-    //sort from lowest to highest and slice to get the two lowest
-    const lowestDiffs = scoreDiffsArr.sort((a, b) => a - b).slice(0, 2);
-    // console.log("LOWEST DIFFS", lowestDiffs);
+    //if this is the players first round start them with score diff of 0
+    if (scoreDiffsArr.length === 0) {
+      scoreDiffsArr.push(0);
+    }
+    console.log("SCORE DIFFS ARRAY", scoreDiffsArr);
 
-    const playerIndex = (
-      lowestDiffs.reduce((a, b) => a + b, 0) / lowestDiffs.length
-    ).toFixed(1);
-    // console.log("PLAYER INDEX", playerIndex);
+    // if the round is completed, these values will be reassigned in the "if" block
+    let scoreDifferential = null;
+    let netStrokes = null;
+    let playerIndex;
+    let courseHandicap;
 
-    /** Compute the course_handicap
-     * (player_index * (course_slope)) / 113
-     *
-     */
-    const courseHandicap = Math.round((playerIndex * courseSlope) / 113);
-    // console.log("COURSE HANDICAP", courseHandicap);
+    ///////////////// IF THE ROUND IS FULLY COMPLETED ///////////////////////
+    /******** calculate the score differential and net strokes  ******/
+    if (
+      Object.values(strokes).every((val) => val !== null) &&
+      Object.values(putts).every((val) => val !== null)
+    ) {
+      /** Compute score_differential = (113 / course_slope) * (total_strokes - course_rating) */
+      scoreDifferential = +(
+        (113 / courseSlope) *
+        (totalStrokes - courseRating)
+      ).toFixed(1);
+      // console.log("SCORE DIFFERENTIAL", scoreDifferential);
 
-    /** Compute the net_strokes :
-     * (total_strokes - course_handicap)
-     */
+      //If player has less than 4 previous rounds but more than 0, push current round scoreDifferential onto scoreDiffsArr to calculate a player index
+      if (scoreDiffsArr.length > 0 && scoreDiffsArr.length < 4) {
+        scoreDiffsArr.push(scoreDifferential);
+      }
+      console.log("SCORE DIFFS ARRAY", scoreDiffsArr);
 
-    const netStrokes = totalStrokes - courseHandicap;
-    // console.log("NET STROKES", netStrokes);
+      //sort from lowest to highest and slice to get the two lowest
+      const lowestDiffs = scoreDiffsArr.sort((a, b) => a - b).slice(0, 2);
+      // console.log("LOWEST DIFFS", lowestDiffs);
 
-    // Update all computed data into rounds table
+      playerIndex = (
+        lowestDiffs.reduce((a, b) => a + b, 0) / lowestDiffs.length
+      ).toFixed(1);
+      console.log("PLAYER INDEX", playerIndex);
+
+      /** Compute the course_handicap = (player_index * (course_slope)) / 113 ***/
+      courseHandicap = Math.round((playerIndex * courseSlope) / 113);
+      console.log("COURSE HANDICAP", courseHandicap);
+
+      /** Compute the net_strokes = (total_strokes - course_handicap) ***/
+      netStrokes = totalStrokes - courseHandicap;
+      // console.log("NET STROKES", netStrokes);
+    } else {
+      ///////////////// IF THE ROUND IS ONLY PARTIALLY COMPLETED ///////////////////////
+      /******** leave score differential and net strokes as null ******/
+
+      //sort from lowest to highest and slice to get the two lowest
+      const lowestDiffs = scoreDiffsArr.sort((a, b) => a - b).slice(0, 2);
+      console.log("LOWEST DIFFS", lowestDiffs);
+
+      playerIndex = (
+        lowestDiffs.reduce((a, b) => a + b, 0) / lowestDiffs.length
+      ).toFixed(1);
+      console.log("PLAYER INDEX", playerIndex);
+
+      /** Compute the course_handicap = (player_index * course_slope) / 113 ***/
+      courseHandicap = Math.round((playerIndex * courseSlope) / 113);
+      console.log("COURSE HANDICAP", courseHandicap);
+    }
+
+    /*** ALL TABLE UPDATES HAPPEN BELOW THE CONDITIONALS THAT COMPUTE ROUND COLUMN VALUES */
+    /*** Update all computed data into rounds table ***/
     const roundRes = await db.query(
       `UPDATE rounds
                SET total_strokes=$1, total_putts=$2, score_differential=$3, player_index=$4, course_handicap=$5, net_strokes=$6
@@ -554,30 +569,24 @@ class Round {
 
     const round = roundRes.rows[0];
 
-    if (!round) throw new NotFoundError(`No round id: ${id}`);
-
+    /*** Insert all the strokes for the round ***/
     const strokesRes = await db.query(
       `UPDATE strokes SET hole1=$1, hole2=$2, hole3=$3, hole4=$4, hole5=$5, hole6=$6, hole7=$7, hole8=$8, hole9=$9, hole10=$10, hole11=$11, hole12=$12, hole13=$13, hole14=$14, hole15=$15, hole16=$16, hole17=$17, hole18=$18
           WHERE round_id=$19
           RETURNING hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18`,
-      [...Object.values(data.strokes), round.id]
+      [...Object.values(strokes), round.id]
     );
 
-    const strokes = strokesRes.rows[0];
-
-    round.strokes = strokes;
-
+    /*** Insert all the putts for the round ***/
     const puttsRes = await db.query(
       `UPDATE putts SET hole1=$1, hole2=$2, hole3=$3, hole4=$4, hole5=$5, hole6=$6, hole7=$7, hole8=$8, hole9=$9, hole10=$10, hole11=$11, hole12=$12, hole13=$13, hole14=$14, hole15=$15, hole16=$16, hole17=$17, hole18=$18
           WHERE round_id=$19
           RETURNING hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18`,
-      [...Object.values(data.putts), round.id]
+      [...Object.values(putts), round.id]
     );
 
-    const putts = puttsRes.rows[0];
-
-    round.putts = putts;
-
+    round.strokes = strokesRes.rows[0];
+    round.putts = puttsRes.rows[0];
     return round;
   }
 

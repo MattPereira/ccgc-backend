f4a40f4129edbc6acf3e27465f7124c27bf3ee00
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-02 16:07:58 -0700

    checking process.env

--- config.js
+++ config.js
@@ -22,6 +22,7 @@ function getDatabaseUri() {
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("CCGC Config:".green);
+console.log("PROCESS.ENV", process.env.NODE_ENV);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);

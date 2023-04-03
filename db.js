e5b7f2b4220d14ec1c160e9184ab7f2085a29b64
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-02 18:13:38 -0700

    revert db.js file to state before try/catch and logs

--- db.js
+++ db.js
@@ -4,34 +4,20 @@ const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
 let db;
-try {
-  if (process.env.NODE_ENV === "production") {
-    db = new Client({
-      connectionString: getDatabaseUri(),
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-  } else {
-    db = new Client({
-      connectionString: getDatabaseUri(),
-      //trying to fix ssl error. remove if it doesn't work
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      // end fix attempt
-    });
-  }
-} catch (err) {
-  console.log("THIS IS CREATING NEW DB CLIENT INSTANCE: ", err);
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+  });
 }
 
-console.log("HELLO WORLD");
-
-try {
-  db.connect();
-} catch (e) {
-  console.log("THIS IS CRASHING SERVER ERROR: ", err);
-}
+db.connect();
 
 module.exports = db;

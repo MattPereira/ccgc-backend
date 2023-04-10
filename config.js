ca601e548809750d44ff0cfd25ae9079a7843401
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-09 19:05:03 -0700

    testing .env file

--- config.js
+++ config.js
@@ -17,16 +17,14 @@ function getDatabaseUri() {
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("CCGC Config:".green);
-console.log("PROCESS.ENV", process.env.NODE_ENV);
+console.log("PROCESS.ENV.ANSWER", process.env.ANSWER);
+console.log("NODE_ENV", process.env.NODE_ENV);
 console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
+// console.log("Database:".yellow, getDatabaseUri());
+console.log("-----");
 
 module.exports = {
   SECRET_KEY,

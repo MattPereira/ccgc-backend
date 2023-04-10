6e382c91096e24e3568bc2fe73027df5d4820a86
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-10 11:32:20 -0700

    remove test env var

--- config.js
+++ config.js
@@ -9,9 +9,6 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
-//Testing remove later
-process.env.HAPPY = process.env.HAPPY || "local";
-
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
@@ -22,12 +19,12 @@ function getDatabaseUri() {
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("CCGC Config:".green);
+console.log("CONFIGURATION LOGS".green);
+console.log("-----");
 console.log("NODE_ENV", process.env.NODE_ENV);
 console.log("PORT:".yellow, PORT.toString());
 console.log("Database:".yellow, getDatabaseUri());
 console.log("-----");
-console.log("HAPPY", process.env.HAPPY);
 
 module.exports = {
   SECRET_KEY,

01246cce34b08a1c8fda576ce5b0ebb0cdaf3f5f
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-10 11:17:15 -0700

    test process.env.HAPPY

--- config.js
+++ config.js
@@ -9,6 +9,9 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
+//Testing remove later
+process.env.HAPPY = process.env.HAPPY || "local";
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
@@ -21,10 +24,10 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("CCGC Config:".green);
 console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("Database:".yellow, getDatabaseUri());
 console.log("-----");
+console.log("HAPPY", process.env.HAPPY);
 
 module.exports = {
   SECRET_KEY,

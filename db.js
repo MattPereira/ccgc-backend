91af04ce71fe6d5c877292f7f1c9e9d92d4be8d9
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-04-18 21:35:46 -0700

    back to basic pg connection logic

--- db.js
+++ db.js
@@ -6,55 +6,27 @@ const { getDatabaseUri } = require("./config");
 // Create a pg.Client instance
 let db;
 
-/**** Connect to db and handle errors ****/
-// db.connect((err) => {
-//   if (err) {
-//     console.log("db connection error 💔", err.stack);
-//     console.error("STACK", err.stack);
-//   } else {
-//     console.log("db connected 🚀");
-//   }
-// });
+db = new Client({
+  connectionString: getDatabaseUri(),
+  connectionTimeoutMillis: 10000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-const connectClient = () => {
-  try {
-    db = new Client({
-      connectionString: getDatabaseUri(),
-      connectionTimeoutMillis: 10000,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-
-    db.connect((err) => {
-      if (err) {
-        console.log("db connection error 💔", err.stack);
-        console.error("STACK", err.stack);
-      } else {
-        console.log("db connected 🚀");
-      }
-    });
-  } catch (err) {
-    console.error("Failed to connect to the database 💔", err);
+db.connect((err) => {
+  if (err) {
+    console.log("db connection error 💔", err.stack);
     console.error("STACK", err.stack);
+  } else {
+    console.log("db connected 🚀");
   }
-};
+});
 
 // Catching errors with listener attatched to Client. https://node-postgres.com/apis/client#events
-if (db) {
-  db.on("error", async (err) => {
-    console.error("Unexpected error on idle client 🫠", err.stack);
 
-    console.log("Ending the database connection 🛑");
-    await db.end();
-
-    console.log("DB VAR", db);
-
-    console.log("Reconnecting to the database 🤙");
-    connectClient();
-  });
-}
-
-connectClient();
+db.on("error", async (err) => {
+  console.error("Unexpected error on idle client 🫠", err.stack);
+});
 
 module.exports = db;
